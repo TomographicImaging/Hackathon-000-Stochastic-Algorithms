909fe943acf2bc2579bca050751936523b50eff7
samdporter <92305641+samdporter@users.noreply.github.com>
2022-06-08 18:32:55 +0100

    Quadratic Prior with proximal inner iter
    Also option for OSL proximal in STIR, but needs exposing

--- cil/QuadPrior.py
+++ cil/QuadPrior.py
@@ -1,22 +1,12 @@
 import sirf.STIR
-import cil.optimisation.algorithms as algorithms
-from cil.optimisation.functions import Function, L2NormSquared
+from cil.optimisation.algorithms import GD
+from cil.optimisation.functions import Function, L2NormSquared, SumScalarFunction
 
-Class QuadraticPrior(Function):
+class Prior(Function):
+    """ Prior Base Class """
     
-    def __init__(self, beta, OSL = False, kappa=None, max_inner_iter = 5):
-        
-        if beta is not None:
-            set_up(beta, OSL, kappa)
-            
-    def set_up(self, beta, OSL, kappa, max_inner_iter) 
-        self.beta = beta # prior weighting
-        self.kappa = kappa # anisotropic pixel weighting image - not existing as yet
-        self.OSL = OSL
-        
-        self.iter = max_inner_iter
-        
-        self.prior = sirf.STIR.QuadraticPrior()
+    def __init__(self, prior, beta):
+        self.prior = prior
         self.prior.set_penalisation_factor(beta)
         
     def __call__(self, image):
@@ -25,14 +15,38 @@ Class QuadraticPrior(Function):
     def gradient(self, image):
         return self.prior.get_gradient(image)
     
+    def set_up(self, image):
+        self.prior.set_up(image)
+
+class QuadraticPrior(Prior):
+    
+    def __init__(self, beta, tau = None, OSL = False, kappa=None, max_inner_iter = 5):
+        
+        if beta is not None:
+            self.set_up(beta, tau, OSL, kappa, max_inner_iter)
+            
+    def set_up(self, beta, tau, OSL, kappa, max_inner_iter):
+        self.beta = beta # prior weighting
+        self.kappa = kappa # anisotropic pixel weighting image - not existing as yet
+        self.OSL = OSL
+        
+        if tau is None:
+            tau = 1
+            print("warning - tau set to 1")
+        self.tau = tau
+        
+        self.iter = max_inner_iter
+        
+        self.prior = Prior(prior = sirf.STIR.QuadraticPrior(), beta=self.beta)
+    
     def proximal(self,image):
-        if OSL is True:
+        self.prior.set_up(image)
+        if self.OSL is True:
             raise NotImplementedError("Not yet exposed")
-            return self.prior.get_proximal
+            return self.prior.get_OSL_proximal(image, tau)
         else:
             g = L2NormSquared(b=image)
-            algorithm = FISTA(max_iteration=self.iter)
-            algorithm.set_up(initial = image, f=self.prior, g=g)
+            algorithm = GD(image, g+self.prior, max_iteration=self.iter)
             algorithm.run(verbose=1)
             return algorithm.solution     
         
